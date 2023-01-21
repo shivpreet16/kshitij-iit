da3c4f8562310d2204d370c71b02aca113b88f37
shivpreet <shivpreet16@gmail.com>
2023-01-21 12:15:20 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+const Page = () => (
+  <div>
+    
+  </div>
+);
+
+export default Page;
